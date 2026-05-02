a368743c113a1726b78e7fde4cc97c954fe948d6
aman <tomaraman50@gmail.com>
2026-05-02 14:35:41 +0530

    fix: increase API rate limit to enhance request handling

--- index.js
+++ index.js
@@ -39,7 +39,7 @@ app.use(cookieParser());
   
 const apiLimiter = rateLimit({
   windowMs: 15 * 60 * 1000,
-  max: Number(process.env.API_RATE_LIMIT_MAX || 100),
+  max: Number(process.env.API_RATE_LIMIT_MAX || 1000),
   standardHeaders: true,
   legacyHeaders: false,
   message: 'Too many requests from this IP, please try again after 15 minutes',
